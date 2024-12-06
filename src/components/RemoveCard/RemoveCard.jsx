9e8b8affaed02e0e0f2cd8c59f87a5867c7f34d7
Jesus Meola <jesusmeola21@gmail.com>
2024-12-06 17:02:12 +0100

    Avance Proyecto 13

---
+++ src/components/RemoveCard/RemoveCard.jsx
@@ -0,0 +1,10 @@
+export default function RemoveCard(props) {
+  return (
+    <div>
+      <h3 class="popup__header">¿Estás seguro?</h3>
+      <button class="popup__button popup__button-create" type="button">
+        Sí
+      </button>
+    </div>
+  );
+}
